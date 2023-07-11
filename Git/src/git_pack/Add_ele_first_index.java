2c1e3d9e2d0e2c103531285880078715adc178c5
malothuswathi <swathimaloth404@gmail.com>
2023-07-11 16:22:35 +0530

    add element first index
    add element first index

---
+++ Git/src/git_pack/Add_ele_first_index.java
@@ -0,0 +1,40 @@
+package git_pack;
+
+
+
+	import java.util.Scanner;
+
+	public class Add_ele_first_index {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc= new Scanner(System.in);
+			System.out.println("Enter the length of the array");
+			int length = sc.nextInt();
+			int arr[]=new int [length];
+					System.out.println("Enter the elements of the array");
+			for(int i=0;i<length;i++)
+			{
+				arr[i]=sc.nextInt();
+			}
+			System.out.println("Enter a new element");
+			int ele=sc.nextInt();
+			for(int i=length-1;i>0;i--)
+			{
+				arr[i]=arr[i-1];
+			}
+			arr[0]=ele;
+			System.out.println("new array is: ");
+			for(int i=0;i<length;i++)
+			{
+				System.out.print(arr[i]+" ");
+			}
+			
+			
+
+		}
+
+	}
+
+
+
