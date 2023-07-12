0cbdfe5acf0497cbbf6ab74c0f39aad621b2494d
malothuswathi <swathimaloth404@gmail.com>
2023-07-12 15:43:59 +0530

    commit

---
+++ Git/src/git_pack/demo.java
@@ -0,0 +1,15 @@
+package git_pack;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=12;
+		int b=30;
+		int c=a+b;
+		System.out.println(c);
+				
+
+	}
+
+}
