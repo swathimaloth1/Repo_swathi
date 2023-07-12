ef760005dde9e3b5bd43bcf7e3b67d30f01b291d
malothuswathi <swathimaloth404@gmail.com>
2023-07-12 11:40:30 +0530

    commit
    Lift shift program

---
+++ Git/src/git_pack/Left_shift.java
@@ -0,0 +1,32 @@
+package git_pack;
+import java.util.Scanner;
+public class Left_shift {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the length of the array");
+		int length = sc.nextInt();
+		int arr[]=new int [length];
+		
+		System.out.println("Enter the elements of the array ");
+		for(int i=0;i<length;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		
+		int frist=arr[0];
+		for(int i=0;i<length-1;i++)
+		{
+			arr[i]=arr[i+1];
+		}
+			arr[length-1]=frist;
+		
+			System.out.println("array after left rotation");
+			for(int i=0;i<length;i++)
+			{
+				System.out.print(arr[i]+" ");
+			}
+		
+			 
+		}
+
+}
