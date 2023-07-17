71053e8f1445ba7ab4b4d5167ae311aca23cc55e
malothuswathi <swathimaloth404@gmail.com>
2023-07-17 11:26:41 +0530

    commit
    updated remove duplicate elements program

--- Git/src/git_pack/Remove_dup_ele.java
+++ Git/src/git_pack/Remove_dup_ele.java
@@ -8,38 +8,37 @@ public class Remove_dup_ele {
 		System.out.println("Enter the length of the array");
 		int length = sc.nextInt();
 		int arr[]=new int [length];
-				System.out.println("Enter the elements of the array");
-		for(int i=0;i<arr.length;i++)
+		
+		System.out.println("Enter the elements of the array");
+		for(int i=0;i<length;i++)
 		{
 			arr[i]=sc.nextInt();
-			
 		}
-		System.out.println("The duplicate elements are");
 		int count=0;
 		for(int i=0;i<length;i++)
 		{
 			count=0;
-			for(int j=i+1;j<length;j++)
+			for(int j=i;j<length;j++)
 			{
-				if(arr[i]==arr[j]) 
-			{
-				count++;
-			
-		    for(int k=i-1;k>=0;k--)
-		    {
-		    	if(arr[i]==arr[k])
-		    	{
-		    		count--;
-		    	}
-		    }
-		   }
+				if(arr[i]==arr[j])
+				{
+					count++;
+				
+				for(int k=i-1;k>=0;k--)
+				{
+					if(arr[i]==arr[k])
+					{
+						count--;
+					}
+				}
+			}
 		}
-		if(count>=1)
+		if(count>0)
 		{
-		
-		System.out.println(arr[i]);
-		}
-		}
+			System.out.println(arr[i]);
+			
 		}
 
+	}
+	}
 }
