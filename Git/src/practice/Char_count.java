e360af0126842787083e58071a662fff02d2c7ce
malothuswathi <swathimaloth404@gmail.com>
2023-07-18 11:53:41 +0530

    commit

---
+++ Git/src/practice/Char_count.java
@@ -0,0 +1,24 @@
+package practice;
+import java.util.Scanner;
+public class Char_count {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the sentence");
+		String str=sc.nextLine();
+		char arr[]= str.toCharArray();
+		int count=0;
+		System.out.println("The character count is: ");
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]!=' ')
+			{
+				count++;
+			}
+		
+		}
+		System.out.println(count);
+		}
+
+}
