c87c8c6e34dabe65d03d508852ea032e4cd6341c
malothuswathi <swathimaloth404@gmail.com>
2023-07-28 19:13:18 +0530

    commit

---
+++ Selenium/src/selenium1/Example_program.java
@@ -0,0 +1,11 @@
+package selenium1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Example_program {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+	}
+
+}
