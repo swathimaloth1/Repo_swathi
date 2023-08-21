bded900ea316868b80dbea94deedf90716879e65
malothuswathi <swathimaloth404@gmail.com>
2023-08-21 12:37:43 +0530

    commit

--- AbhiBus/src/abhibus/Homepage.java
+++ AbhiBus/src/abhibus/Homepage.java
@@ -2,14 +2,13 @@ package abhibus;
 
 import java.util.Set;
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Homepage extends Base {
-	
-		
-	
+	             
 	public static void main(String[] args) throws InterruptedException {
 		driver = new ChromeDriver();
 		driver.get("https://www.abhibus.com/");
@@ -22,28 +21,31 @@ public class Homepage extends Base {
 		WebElement ele = driver.findElement(by_bus);
 	    ele.sendKeys("Hyderabad");
 	    
-	    By by_train = By.id("pills-profile-tab");
-		  WebElement ele1 = driver.findElement(by_train);
+	    By by_busoffers = By.xpath("//img[@title='Save Rs.300 on APSRTC bookings']");
+		  WebElement ele1 = driver.findElement(by_busoffers);
 		  ele1.click();
 	    
 	   Set <String> handles = driver.getWindowHandles();
 	   System.out.println(handles);
 	   for(String handle : handles)
 	   {
-		   System.out.println(handle);
+		  
 		   if(!handle.equals(parenthandle))
 		   {
 			  driver.switchTo().window(handle) ;
 			 
 			  
 			  	Thread.sleep(2000);
-			  	//driver.quit();
+			  	 driver.close();
+			  	
 		   }
 		   
+		  
 	   }
+	  
 	}
-}	
-
+	
+}
 	
 
 
