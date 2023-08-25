98b7c5b8f6c52b0b99b00f6fb526d1b6c481934c
malothuswathi <swathimaloth404@gmail.com>
2023-08-25 18:54:27 +0530

    commit

--- OrangeHRM/src/orageHRM/Login.java
+++ OrangeHRM/src/orageHRM/Login.java
@@ -4,28 +4,46 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class Login extends Base{
-	 By by_username=By.xpath("//input[@name='username']");
-	 By by_password=By.xpath("//input[@name='password']");
-	 By by_loginbtn=By.xpath("//button[@type='submit']"); 
+	 static By by_username=By.xpath("//input[@name='username']");
+	static  By by_password=By.xpath("//input[@name='password']");
+	static  By by_loginbtn=By.xpath("//button[@type='submit']"); 
 	
-	  WebElement by_uname = driver.findElement(by_username);
-	  WebElement by_pw = driver.findElement(by_password);
-	  WebElement bt_login = driver.findElement(by_loginbtn);
-	
-	public  void username() throws InterruptedException
+	 
+	 
+	  
+	public static WebElement uname()
 	{
-		by_uname.sendKeys("Admin");
+		 WebElement by_uname = driver.findElement(by_username);
+		return  by_uname;
+		
+	}
+	
+	public static WebElement pass()
+	{
+		 WebElement by_pw = driver.findElement(by_password);
+		return by_pw;
+		
+	}
+	
+	public static WebElement l()
+	{
+		WebElement bt_login = driver.findElement(by_loginbtn);
+		return bt_login;
+	}
+	public static  void username() throws InterruptedException
+	{
+		uname().sendKeys("Admin");
 		Thread.sleep(2000);
 	}
 	
-	public  void password() throws InterruptedException
+	public static void password() throws InterruptedException
 	{
-		by_pw.sendKeys("admin123");
+		pass().sendKeys("admin123");
 		Thread.sleep(2000);
 	}
-	public  void loginbutton() throws InterruptedException
+	public static void loginbutton() throws InterruptedException
 	{
-		bt_login.click();
+		l().click();
 		Thread.sleep(2000);
 	}
 	
--- OrangeHRM/src/orageHRM/Main.java
+++ OrangeHRM/src/orageHRM/Main.java
@@ -5,18 +5,15 @@ public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 		Base.browser();
-		Login l = new Login();
-		l.username();
-		l.password();
-		l.loginbutton();
 		
-		Logout la=new Logout();
-		la.logout();
-		la.logoutbtn();
+		Login.username();
+		Login.password();
+		Login.loginbutton();
 		
-//		Login.username();
-//		Login.password();
-//		Login.loginbutton();
+		Logout.logoutbtn();
+		Logout.logout();
+		
+		
 	
 		                                                                                
 }
